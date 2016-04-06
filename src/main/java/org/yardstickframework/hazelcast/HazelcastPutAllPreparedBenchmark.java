6adf7b9b7b0d03f946c59c2a55e75e5854688b2d
donnerbart <github@thunderphreak.de>
2016-04-06 22:55:04 +0200

    Fixed HazelcastPutAllPreparedBenchmark. Created separate benchmark.properties for putAll tests.

--- src/main/java/org/yardstickframework/hazelcast/HazelcastPutAllPreparedBenchmark.java
+++ src/main/java/org/yardstickframework/hazelcast/HazelcastPutAllPreparedBenchmark.java
@@ -43,11 +43,11 @@ public class HazelcastPutAllPreparedBenchmark extends HazelcastAbstractMapBenchm
 
         for (int i = 0; i < PREPARED_MAPS_COUNT; i++) {
             SortedMap<Integer, Integer> valueMap = new TreeMap<>();
-            do {
+            for (int j = 0; j < args.batch(); j++) {
                 int key = nextRandom(args.range());
 
                 valueMap.put(key, key);
-            } while (valueMap.size() < args.batch());
+            }
             maps.add(valueMap);
         }
         mapsSize = maps.size();
